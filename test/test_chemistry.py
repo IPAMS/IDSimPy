b02be25e7553e3b6a83a51a43639d027321d149d
Walter Wissdorf <mail@walterwissdorf.de>
2019-04-16 02:49:50 +0200

    Add basic test for chemistry analysis

---
+++ test/test_chemistry.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import IDSimF_analysis as ia
+import IDSimF_analysis.visualization as vis
+
+
+class TestChemistry(unittest.TestCase):
+
+	@classmethod
+	def setUpClass(cls):
+		cls.test_concentrations_a = os.path.join('data', 'qitSim_2019_04_scanningTrapTest',
+		                                          'qitSim_2019_04_10_001_concentrations.txt')
+		cls.test_concentrations_b = os.path.join('data', 'qitSim_2019_04_scanningTrapTest',
+		                                          'qitSim_2019_04_10_002_concentrations.txt')
+		cls.result_path = "test_results"
+
+	def test_basic_concentration_plotting(self):
+		ia.plot_concentration_file(self.test_concentrations_a)
+		plt.savefig(os.path.join(self.result_path,'qitSim_2019_04_10_001_concentrations.pdf'))
+		plt.figure()
+		ia.plot_concentration_file(self.test_concentrations_b)
+		plt.savefig(os.path.join(self.result_path, 'qitSim_2019_04_10_002_concentrations.pdf'))
