deb093aabb62a9fc53dc70cf38bd24102efab0ff
Walter Wissdorf <mail@walterwissdorf.de>
2021-08-30 03:19:44 +0200

    Adds a script to run test simulations with IDSimF

---
+++ test/analysis/run_integration_test_simulations.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+Run test simulations in IDSimF to generate current trajectory files
+"""
+
+import sys
+import os.path as path
+import subprocess
+
+def main():
+    print("hello")
+    print(sys.argv[1])
+
+    idsimf_build_base_path = sys.argv[1]
+
+    # BT-QITSim executable:
+    qit_sim_exe = path.join(idsimf_build_base_path, 'applications','ionTraps','BT-QITSim', 'BT-QITSim')
+    sim_input = path.join('.', 'integration', 'inputs', 'qitSim_2019_07_22_001_conf.json')
+    sim_result_path = path.join('.', 'integration', 'sim_results', 'qitSim_2019_07_22_001')
+
+    subprocess.run(qit_sim_exe+' '+sim_input+' '+sim_result_path, shell=True, check=True)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
