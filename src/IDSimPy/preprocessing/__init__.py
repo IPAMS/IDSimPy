a749b553ab27f357b224f63d30893dc7dad59da6
Walter Wissdorf <mail@walterwissdorf.de>
2022-08-19 17:24:15 +0200

    Adds a simple function to generate input files

--- src/IDSimPy/preprocessing/__init__.py
+++ src/IDSimPy/preprocessing/__init__.py
@@ -7,3 +7,10 @@ Modules:
   * ion_cloud_generation: Generation of ion cloud initialization files
   * field_generation: Generation / Transformation of scalar and vector fields
 """
+
+from . import comsol_import
+from . import field_generation
+from . import input_file_preprocessing
+from .input_file_preprocessing import generate_input_files_from_template
+
+from . import ion_cloud_generation
---
+++ src/IDSimPy/preprocessing/input_file_preprocessing.py
@@ -0,0 +1,38 @@
+
+def generate_input_files_from_template(template_filename, parameter_values, result_basename):
+    """
+    Generates simulation input files from a simple template file,
+    a vector of parameter values and a basename for the resulting input files
+
+    A template file is a simple text file, which contains replacement tokens of the form
+    `%%-0-%%` with a running index
+
+    Parameter values are a list of parameter value lists, with one parameter value vector per resulting file
+
+    Example:
+
+    parameters = (
+        (result_file_1_parameter_1, result_file_1_parameter_2,),
+        (result_file_2_parameter_1, result_file_2_parameter_2),
+        (result_file_2_parameter_1, result_file_2_parameter_2)
+    )
+
+    :param template_filename: Name of the template file
+    :param parameter_values: list List of parameter values
+    :param result_basename: Basename of the resulting generated input files
+    """
+    with open(template_filename) as template_file:
+        template = template_file.read()
+
+        for i, pv in enumerate(parameter_values):
+            if not isinstance(pv, (list, tuple)):
+                pv = (pv,)
+
+            buf = template
+            for vi, value in enumerate(pv):
+                buf = buf.replace('%%-{:d}-%%'.format(vi), str(value))
+
+            result_str = buf
+            result_filename = result_basename + '{:03d}.json'.format(i)
+            with open(result_filename, 'w') as result_file:
+                result_file.write(result_str)
