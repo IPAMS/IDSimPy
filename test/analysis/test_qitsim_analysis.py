02ac5f218d7fbfbedfaa8ffaac9c0216b7c9cc18
Walter Wissdorf <mail@walterwissdorf.de>
2024-03-15 01:44:05 +0100

    Corrects z-dimension phase space trajectory plots

--- test/analysis/test_qitsim_analysis.py
+++ test/analysis/test_qitsim_analysis.py
@@ -121,7 +121,10 @@ class TestQitSimAnalysis(unittest.TestCase):
 
 		tra = qa.read_trajectory_file_for_project(self.sim_ft_qit_2, 'hdf5')
 
-		pdef = [10, 20]
-		fig = qa.plot_phase_space_trajectory(tra, pdef, xlim=(-0.001, 0.001))
+		pdef = [
+			{'i':10, 'alpha':0.5},
+			{'i':20, 'cl':'red'},
+			{'i':21, 'cl':'#FFAABB'}]
+		fig = qa.plot_phase_space_trajectory(tra, pdef, xlim=(-0.005, 0.005))
 
 		fig.show()
